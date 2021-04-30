6f998d37967487cc224ae435614b1fb708512021
Dave86dev <davidochando1986@gmail.com>
2021-04-30 13:41:18 +0200

    Primer intento, 20 min

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,22 @@
 function tiers(index) {
-	return "";
+
+	let linea = "";
+	let dibujo = "";
+
+	for(let i = 1; i <= index; i++){
+		for(let x = 1; x <= (index - i); x++){
+			line += " ";
+		}
+
+		for(let z = 1; z <= i; z++){
+			line += "*";
+		}
+
+		dibujo += linea;
+		dibujo += "\n";
+	}
+
+	return dibujo;
 }
 module.exports = tiers;
+
